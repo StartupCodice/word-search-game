a0959b65896a64f32bba3d60b83992fa747e90ec
Calvin soares <calvinsoares17@gmail.com>
2023-11-05 12:35:06 -0300

    Style: aplicação de estilos nos popUps

--- src/pages/ScreenJogar/Jogar.js
+++ src/pages/ScreenJogar/Jogar.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { View, Text, TouchableHighlight, TouchableOpacity, ImageBackground } from "react-native";
-import styles from "../Home/style";
+import React, {useState} from 'react';
+import { View, Text, Modal, Alert, Pressable, TouchableHighlight, TouchableOpacity, ImageBackground } from "react-native";
+import styles from './style';
 
 export function Jogar() {
-    return (
-        <View style={styles.container}>
-                <ImageBackground source={require('../../assets/bg3.jpeg')} style={styles.imageBackground}>
+        return (
+          <View style={styles.container}>
+                <ImageBackground source={require('../../assets/bg1.jpeg')} style={styles.imageBackground}>
                     <View style={styles.buttonContainer}>
                        <Text style={styles.buttonTextRed}>
-                            batata assada
+                            batata
                         </Text> 
                     </View>    
                 </ImageBackground>
             </View>
-    )
-}
+        );
+      };    
