d7c4b86fe847519fd03ae3d7ccb22f103061a2f1
Calvin soares <calvinsoares17@gmail.com>
2023-11-05 17:03:29 -0300

    fix: problema de retorno na rota jogofacil

--- src/components/ModalEvento.js
+++ src/components/ModalEvento.js
@@ -9,13 +9,14 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from "../pages/Home/style";
+import { JogoFacil } from "../pages/JogoFacil/JogoFacil";
 
 
-export function ModalEvento(){
+export function ModalEvento({ navigation }){
     const [modalVisible, setModalVisible] = useState(false);
     return (
 
-        <View style={styles.centeredView}>
+        <View style={styles.centeredView} >
                             <Modal
                             animationType="fade"
                             transparent={true}
@@ -26,23 +27,40 @@ export function ModalEvento(){
                             }}>
                             <View style={styles.centeredView}>
                                 <View style={styles.modalView}>
-
+                                <View style={styles.header}>
+                                    <Text style={styles.textHeader}>
+                                        JOGOS DIARIOS
+                                    </Text>
+                                </View>
                                 <TouchableHighlight
-                                    style={styles.buttonInside}
-                                    underlayColor="null"
-                                    onPress={() => setModalVisible(!modalVisible)}>
-                                    <Ionicons style={styles.textStyle} name="close-sharp" size={36} color="black" />
-                                </TouchableHighlight>
+                                        style={styles.buttonInsideEvento}
+                                        underlayColor="null"
+                                        onPress={() => setModalVisible(!modalVisible)}>
+                                        <Ionicons style={styles.textStyle} name="close-sharp" size={36} color="black" />
+                                    </TouchableHighlight>
                                 <Pressable
+                                    underlayColor="null"            
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={ () => navigation.navigate('JogoFacil') }>
+                                    <Text style={styles.textStyleJogos}>FÁCIL</Text>
+                                </Pressable>
+                                <Pressable
+                                    underlayColor="null"            
                                     style={[styles.button, styles.buttonClose]}
                                     onPress={() => setModalVisible(!modalVisible)}>
-                                    <Text style={styles.textStyle}>Voltar</Text>
+                                    <Text style={styles.textStyleJogos}>MÉDIO</Text>
+                                </Pressable>
+                                <Pressable
+                                    underlayColor="null"            
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={() => setModalVisible(!modalVisible)}>
+                                    <Text style={styles.textStyleJogos}>DÍFICIL</Text>
                                 </Pressable>
                                 </View>
                             </View>
                             </Modal>
                             <Pressable
-                            style={[styles.button, styles.buttonOpen]}
+                            style={[styles.buttonSnow, styles.buttonOpen]}
                             onPress={() => setModalVisible(true)}>
                                 <Ionicons name="snow" size={24} color="white" style={styles.textStyle} />
                             </Pressable>
