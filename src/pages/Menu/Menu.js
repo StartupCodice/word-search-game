116bd62b9552edfea1180ec5169edcec9ee0df62
adryanoliveira23 <blessedt402@gmail.com>
2023-11-21 20:28:04 -0300

    feat: import da tela de Menu

---
+++ src/pages/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, Image, TouchableOpacity, TouchableHighlight, ImageBackground, ScrollView} from 'react-native';
+import styles from './style';
+import { Ionicons } from '@expo/vector-icons';
+
+export function Menu({ navigation }){
+    return(
+        <View>
+            <Text> 
+                Em Breve
+            </Text>
+        </View>
+    )
+}
