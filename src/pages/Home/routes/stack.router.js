e2d9a6d883f5162538c5fca6ca8298d08ae9f6ff
Calvin soares <calvinsoares17@gmail.com>
2023-11-21 22:03:04 -0300

    feat:
    Criação das rotas dos botões de niveis e temas

--- src/pages/Home/routes/stack.router.js
+++ src/pages/Home/routes/stack.router.js
@@ -5,10 +5,14 @@ const { Screen, Navigator } = createNativeStackNavigator();
 import { Ionicons } from '@expo/vector-icons';
 
 import Jogar from '../../ScreenJogar/Jogar';
-import { Estilos } from '../../Estilos/Estilos';
 import { Infinito } from '../../Infinito/Infinito';
 import Home from '../Home';
 import { JogoFacil } from './../../JogoFacil/JogoFacil';
+import NiveisBotoes  from '../../Estilos/NivelBotoes';
+import  { NivelFacil }  from '../../Estilos/NivelFacil/NivelFacil';
+import  { NivelMedio }  from '../../Estilos/NivelMedio/NivelMedio';
+import { NivelDificil } from '../../Estilos/NivelDificil/NivelDificil';
+import { NivelPro } from '../../Estilos/NivelPro/NivelPro';
 
 export function StackRoutes() {
     return (
@@ -40,12 +44,49 @@ export function StackRoutes() {
 
              <Screen 
              name="Estilos"
-             component={Estilos}
+             component={NiveisBotoes}
              options={{ 
                 title: '',
                 headerShown: false       
             }}
              />
+
+            <Screen 
+             name="NivelFacil"
+             component={NivelFacil}
+             options={{ 
+                title: '',
+                headerShown: false       
+            }}
+             />
+
+            <Screen 
+             name="NivelMedio"
+             component={NivelMedio}
+             options={{ 
+                title: '',
+                headerShown: false       
+            }}
+             />
+            
+            <Screen 
+             name="NivelDificil"
+             component={NivelDificil}
+             options={{ 
+                title: '',
+                headerShown: false       
+            }}
+             />
+
+            <Screen 
+             name="NivelPro"
+             component={NivelPro}
+             options={{ 
+                title: '',
+                headerShown: false       
+            }}
+             />
+
 
              <Screen 
              name="Infinito"
