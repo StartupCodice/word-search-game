5acf6ea0201dc448fb08937a6875f7c33f4b94d1
Calvin soares <calvinsoares17@gmail.com>
2023-12-01 21:49:45 -0300

    feat: rota atualizada

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import tw from 'twrnc'
 import { Routes } from './src/pages/Home/routes';
 
-
 export default function App () {
   return (
     <Routes />
