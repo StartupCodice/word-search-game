65f28fdd72fdaa9f6490926c5edf570499d82919
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-12-18 21:05:51 -0300

    Implementado nivel dificil

--- src/pages/Estilos/NivelDificil/TemasJogo/DecoracoesDIificil/DecoracoesDIificil.js
+++ src/pages/Estilos/NivelDificil/TemasJogo/DecoracoesDIificil/DecoracoesDIificil.js
@@ -8,6 +8,7 @@ import randomcolor from 'randomcolor';
 import styles from './style';
 import {scale} from 'react-native-size-matters';
 import MoedasComponent from '../../../../../components/storage';
+import NiveisDificil from '../../../../../components/storageNivelDificil';
 
 import { PanGestureHandler, State, GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -22,6 +23,10 @@ const Cell = React.memo(({ letter, selected }) => (
 
 
 export default function AlimentosDificil({ navigation, rows = 10, cols = 10 }) {
+  const { 
+    decoracoes, 
+    addDecoracoes,
+  } = NiveisDificil();
 
   const [palavras, setPalavras] = useState([]);
   const [board, setBoard] = useState({
@@ -199,6 +204,9 @@ export default function AlimentosDificil({ navigation, rows = 10, cols = 10 }) {
 
     adicionarMoedas(6);
     setMoedasGanhas(6);
+
+    let level = parseInt(decoracoes) + 1;
+    if (decoracoes < 30) addDecoracoes(level.toString());
   
     setModalVisible(true);
     setTempoDecorrido(tempoFormatado);
