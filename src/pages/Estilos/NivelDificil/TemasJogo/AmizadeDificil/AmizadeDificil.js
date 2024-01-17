0ab2e10ce290c3635446596c09e1ed32b4dfc8d9
Lucas-Henschel <lucas101henschel@gmail.com>
2024-01-16 21:11:55 -0300

    Implemented ads on level hard

--- src/pages/Estilos/NivelDificil/TemasJogo/AmizadeDificil/AmizadeDificil.js
+++ src/pages/Estilos/NivelDificil/TemasJogo/AmizadeDificil/AmizadeDificil.js
@@ -10,6 +10,9 @@ import {scale} from 'react-native-size-matters';
 import MoedasComponent from '../../../../../components/storage';
 import NiveisDificil from '../../../../../components/storageNivelDificil';
 import { PanGestureHandler, State, GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BannerAds } from '../../../../../components/BannerAds';
+import { InterstitialAds } from '../../../../../components/InterstitialAds';
+import { RewardedAds } from '../../../../../components/RewardedAds';
 
 const CELL_SIZE = Math.floor(260 * 0.1);
 const CELL_PADDING = Math.floor(CELL_SIZE * 0.1);
@@ -49,7 +52,9 @@ export default function AmizadeDificil({ navigation, rows = 10, cols = 10 }) {
     endY: 0,
     gestureType: null,
   });
-
+  const [showIntersititial, setShowIntersititial] = useState(false);
+  const [showRewarded, setShowRewarded] = useState(false);
+  const [addTip, setAddTip] = useState(false);
 
   const isMountedRef = useRef(true);
 
@@ -249,6 +254,7 @@ export default function AmizadeDificil({ navigation, rows = 10, cols = 10 }) {
 
   const closeModal = () => {
     reiniciarJogo();
+    setShowIntersititial(true);
   };
 
   const [selectedCells, setSelectedCells] = useState([]);
@@ -317,16 +323,22 @@ const onHandlerStateChange = (event, item) => {
   userWin();
 };
 
-const isAligned = (cell1, cell2) => {
-  if (!cell1 || !cell2) return false;
+  const isAligned = (cell1, cell2) => {
+    if (!cell1 || !cell2) return false;
 
-  const rowDiff = Math.abs(cell1.row - cell2.row);
-  const colDiff = Math.abs(cell1.col - cell2.col);
+    const rowDiff = Math.abs(cell1.row - cell2.row);
+    const colDiff = Math.abs(cell1.col - cell2.col);
 
-  return rowDiff === colDiff || cell1.row === cell2.row || cell1.col === cell2.col;
-};
+    return rowDiff === colDiff || cell1.row === cell2.row || cell1.col === cell2.col;
+  };
 
 
+  const getRewarded = () => {
+    setShowRewarded(false);
+    setNumDicasUsadas(3);
+    setAddTip(true);
+    fecharModalDicasEsgotadas();
+  }
 
   return (
     <View style={styles.container}>
@@ -396,11 +408,18 @@ const isAligned = (cell1, cell2) => {
         </View>
         </View>
 
+        <View style={{ marginTop: scale(200) }}>
+          <BannerAds />
+        </View>
+
         <Modal isVisible={hintsExhausted} onBackdropPress={fecharModalDicasEsgotadas} style={styles.modalContainer2}>
         <View style={styles.modalContainer}>
           <Text style={styles.modalText}>
             As dicas acabaram!
           </Text>
+          <TouchableOpacity style={styles.modalButton} onPress={() => setShowRewarded(true)}>
+            <Text style={styles.modalButtonText}>+1 dica</Text>
+          </TouchableOpacity>
           <TouchableOpacity style={styles.modalButton} onPress={fecharModalDicasEsgotadas}>
             <Text style={styles.modalButtonText}>Fechar</Text>
           </TouchableOpacity>
@@ -428,6 +447,18 @@ const isAligned = (cell1, cell2) => {
           
         </View>
       </Modal>
+
+      {
+        showIntersititial ?
+          <InterstitialAds closeInterstitial={() => setShowIntersititial(false)} />
+        : null
+      }
+
+      {       
+        showRewarded ?
+          <RewardedAds getRewarded={getRewarded} />
+        : null
+      }
 
         <StatusBar style="auto" />
       </ImageBackground>
--- src/pages/Estilos/NivelDificil/TemasJogo/CarrosDificil/CarrosDificil.js
+++ src/pages/Estilos/NivelDificil/TemasJogo/CarrosDificil/CarrosDificil.js
@@ -9,6 +9,9 @@ import styles from './style';
 import {scale} from 'react-native-size-matters';
 import MoedasComponent from '../../../../../components/storage';
 import NiveisDificil from '../../../../../components/storageNivelDificil';
+import { BannerAds } from '../../../../../components/BannerAds';
+import { InterstitialAds } from '../../../../../components/InterstitialAds';
+import { RewardedAds } from '../../../../../components/RewardedAds';
 
 import { PanGestureHandler, State, GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -50,7 +53,9 @@ export default function CarrosDificil({ navigation, rows = 10, cols = 10 }) {
     endY: 0,
     gestureType: null,
   });
-
+  const [showIntersititial, setShowIntersititial] = useState(false);
+  const [showRewarded, setShowRewarded] = useState(false);
+  const [addTip, setAddTip] = useState(false);
 
   const isMountedRef = useRef(true);
 
@@ -294,6 +299,7 @@ export default function CarrosDificil({ navigation, rows = 10, cols = 10 }) {
 
   const closeModal = () => {
     reiniciarJogo();
+    setShowIntersititial(true);
   };
 
   const [selectedCells, setSelectedCells] = useState([]);
@@ -371,7 +377,12 @@ const isAligned = (cell1, cell2) => {
   return rowDiff === colDiff || cell1.row === cell2.row || cell1.col === cell2.col;
 };
 
-
+const getRewarded = () => {
+  setShowRewarded(false);
+  setNumDicasUsadas(3);
+  setAddTip(true);
+  fecharModalDicasEsgotadas();
+}
 
   return (
     <View style={styles.container}>
@@ -441,11 +452,18 @@ const isAligned = (cell1, cell2) => {
         </View>
         </View>
 
+        <View style={{ marginTop: scale(200) }}>
+          <BannerAds />
+        </View>
+
         <Modal isVisible={hintsExhausted} onBackdropPress={fecharModalDicasEsgotadas} style={styles.modalContainer2}>
         <View style={styles.modalContainer}>
           <Text style={styles.modalText}>
             As dicas acabaram!
           </Text>
+          <TouchableOpacity style={styles.modalButton} onPress={() => setShowRewarded(true)}>
+            <Text style={styles.modalButtonText}>+1 dica</Text>
+          </TouchableOpacity>
           <TouchableOpacity style={styles.modalButton} onPress={fecharModalDicasEsgotadas}>
             <Text style={styles.modalButtonText}>Fechar</Text>
           </TouchableOpacity>
@@ -473,6 +491,18 @@ const isAligned = (cell1, cell2) => {
           
         </View>
       </Modal>
+
+      {
+        showIntersititial ?
+          <InterstitialAds closeInterstitial={() => setShowIntersititial(false)} />
+        : null
+      }
+
+      {       
+        showRewarded ?
+          <RewardedAds getRewarded={getRewarded} />
+        : null
+      }
 
         <StatusBar style="auto" />
       </ImageBackground>
